bfa3aba176a7f14f8b5a7295cb976b8cfc98b3b4
ASDFG0103 <164356870+ASDFG0103@users.noreply.github.com>
2024-03-21 21:28:50 +0800

    Add files via upload

---
+++ JPA01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class hello {
+   public static void main(String[] args){
+      System.out.println("Please enter score");
+      Scanner s = new Scanner(System.in);
+      int a = s.nextInt();
+      if(a>60 || a==60){
+         System.out.println("You pass");
+         System.out.println("End");        
+      }else{
+         System.out.println("End");
+      }
+   }   
+}
